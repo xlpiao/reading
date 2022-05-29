83f155cba9c97c75a9b4bb1edbb31f44f434e9fc
Xianglan Piao <xianglan0502@gmail.com>
2022-05-30 07:57:50 +0900

    Update csv2md.sh

--- csv2md.sh
+++ csv2md.sh
@@ -1,15 +1,15 @@
-input=reading.csv
-output=README.md
+INPUT_FILE=reading.csv
+OUTPUT_FILE=README.md
 
-echo -e "# [Keep reading! There's another world in books!](https://github.com/xlpiao/reading/blob/master/reading.csv)\n" > $output
+echo -e "# [Keep reading! There's another world in books!](https://github.com/xlpiao/reading/blob/master/reading.csv)\n" > $OUTPUT_FILE
 
-skip_header=true
+INPUT_HEADER=true
 while read INPUT; do
-  if $skip_header;then
-    echo -e "| ${INPUT//,/ | } |" >> $output ;
-    echo -e "| --- | --- | --- | --- | --- | --- | --- |" >> $output
-    skip_header=false
+  if $INPUT_HEADER;then
+    echo -e "| ${INPUT//,/ | } |" >> $OUTPUT_FILE ;
+    echo -e "| --- | --- | --- | --- | --- | --- | --- |" >> $OUTPUT_FILE
+    INPUT_HEADER=false
     continue
   fi
-  echo -e "| ${INPUT//,/ | } |" >> $output ;
-done < $input;
+  echo -e "| ${INPUT//,/ | } |" >> $OUTPUT_FILE ;
+done < $INPUT_FILE;
